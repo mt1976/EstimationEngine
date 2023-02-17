9a1d4a444e53d103d1d3a3fa78cfdacc56436751
Matt <mt76@gmx.com>
2023-02-17 14:47:55 +0000

    Create Windows Starter

---
+++ start.bat
@@ -0,0 +1,8 @@
+rem Starts the server
+rem
+set STARTER_APP=main.go
+set STARTER_PATH=C:\Dev\GitHub\ebEstimates
+set APP_PORT=5050
+title EB Estimates Server - %STARTER_APP% - %STARTER_PATH% - %APP_PORT%
+
+exit
