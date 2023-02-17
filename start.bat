d736e05f99e06a8635a28f98d73c9270c5c15a80
Matt <mt76@gmx.com>
2023-02-17 15:28:35 +0000

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,8 +1,18 @@
+@echo off
 rem Starts the server
 rem
-set STARTER_APP=main.go
+set STARTER_APP=./...
 set STARTER_PATH=C:\Dev\GitHub\ebEstimates
-set APP_PORT=5050
-title EB Estimates Server - %STARTER_APP% - %STARTER_PATH% - %APP_PORT%
-
-exit
+rem set APP_PORT=5050
+set DB_SERVER=localhost
+rem set DB_PORT=1433
+set DB_USER=ebEstimates
+set DB_PASSWORD=6LmYqp&*CBzo@s
+set DB_DATABASE=EST_ALPHA
+set STARTER_NAME=EB Estimates Server
+title %STARTER_NAME% - %STARTER_APP% - %STARTER_PATH% - %APP_PORT%
+cls
+echo %STARTER_NAME% - %STARTER_APP% - %STARTER_PATH% - %APP_PORT%
+cd %STARTER_PATH%
+go version
+go run %STARTER_APP%
